85de2c817255cf713f320a28df5c3d95b5847cf0
Roxane <sindbathos@hotmail.fr>
2015-07-01 14:11:50 +0200

    Add a way to analyze an external GFF3 file

---
+++ draw_transcript_ext.sh
@@ -0,0 +1,46 @@
+OUTPUTDIR=$1
+GFFFILE=$2
+SVGFLAG=".svg"
+JSONFLAG=".json"
+SVGOUTPUT1="$OUTPUTDIR""1""$SVGFLAG"
+SVGOUTPUT2="$OUTPUTDIR""2""$SVGFLAG"
+SVGOUTPUT3="$OUTPUTDIR""3""$SVGFLAG"
+JSONOUTPUT1="$OUTPUTDIR""1""$JSONFLAG"
+JSONOUTPUT2="$OUTPUTDIR""2""$JSONFLAG"
+JSONOUTPUT3="$OUTPUTDIR""3""$JSONFLAG"
+
+if [ -d "results" ]; then
+ echo "Création nouveau dossier dans results"
+else
+ mkdir results
+fi
+
+mkdir results/${OUTPUTDIR}
+mkdir results/${OUTPUTDIR}/GFF
+mkdir results/${OUTPUTDIR}/SVG
+mkdir results/${OUTPUTDIR}/JSON
+mkdir results/${OUTPUTDIR}/FASTA
+mkdir results/${OUTPUTDIR}/report
+cp ${GFFFILE} results/${OUTPUTDIR}/GFF/${GFFFILE}
+GFFOUTPUT=${GFFFILE}
+python src/drawIsoforms.py --proportionnal --print-count --annotation results/${OUTPUTDIR}/JSON/structure.json --legend results/${OUTPUTDIR}/JSON/legend.json results/${OUTPUTDIR}/GFF/${GFFOUTPUT} results/${OUTPUTDIR}/JSON/${JSONOUTPUT1}
+python src/drawIsoforms.py --fixed results/${OUTPUTDIR}/GFF/${GFFOUTPUT} results/${OUTPUTDIR}/JSON/${JSONOUTPUT2}
+python src/drawIsoforms.py --listed results/${OUTPUTDIR}/GFF/${GFFOUTPUT} results/${OUTPUTDIR}/JSON/${JSONOUTPUT3}
+cp -R html_things results/${OUTPUTDIR}/report
+cp -R results/${OUTPUTDIR}/FASTA results/${OUTPUTDIR}/report
+cp results/${OUTPUTDIR}/JSON/${JSONOUTPUT1} results/${OUTPUTDIR}/report/html_things/images/${JSONOUTPUT1}
+cp results/${OUTPUTDIR}/JSON/${JSONOUTPUT2} results/${OUTPUTDIR}/report/html_things/images/${JSONOUTPUT2}
+cp results/${OUTPUTDIR}/JSON/${JSONOUTPUT3} results/${OUTPUTDIR}/report/html_things/images/${JSONOUTPUT3}
+cp src/jsonToSvg.js results/${OUTPUTDIR}/report/jsonToSvg.js
+cp results/${OUTPUTDIR}/JSON/structure.json results/${OUTPUTDIR}/report/html_things/images/structure.json
+cp results/${OUTPUTDIR}/JSON/legend.json results/${OUTPUTDIR}/report/html_things/images/legend.json
+sed -e "s|%%AnalysisName%%|${OUTPUTDIR}|g" html_things/jackalope_svg1.html > html_things/jackalope_svg1Inter1.html
+sed -e "s|%%AnalysisTag%%|${JSONOUTPUT1}|g" html_things/jackalope_svg1Inter1.html > html_things/jackalope_svg1Inter2.html
+sed -e "s|%%SVG1%%|html_things/images/${SVGOUTPUT1}|g" html_things/jackalope_svg1Inter2.html > results/${OUTPUTDIR}/report/jackalope_svg1_${OUTPUTDIR}.html
+sed -e "s|%%AnalysisName%%|${OUTPUTDIR}|g" html_things/jackalope_svg2.html > html_things/jackalope_svg2Inter1.html
+sed -e "s|%%AnalysisTag%%|${JSONOUTPUT2}|g" html_things/jackalope_svg2Inter1.html > html_things/jackalope_svg2Inter2.html
+sed -e "s|%%SVG2%%|html_things/images/${SVGOUTPUT2}|g" html_things/jackalope_svg2Inter2.html > results/${OUTPUTDIR}/report/jackalope_svg2_${OUTPUTDIR}.html
+sed -e "s|%%AnalysisName%%|${OUTPUTDIR}|g" html_things/jackalope_svg3.html > html_things/jackalope_svg3Inter1.html
+sed -e "s|%%AnalysisTag%%|${JSONOUTPUT3}|g" html_things/jackalope_svg3Inter1.html > html_things/jackalope_svg3Inter2.html
+sed -e "s|%%SVG3%%|html_things/images/${SVGOUTPUT3}|g" html_things/jackalope_svg3Inter2.html > results/${OUTPUTDIR}/report/jackalope_svg3_${OUTPUTDIR}.html
+firefox results/${OUTPUTDIR}/report/jackalope_svg3_${OUTPUTDIR}.html
